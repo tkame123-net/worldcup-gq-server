ab1e8349e26f0df1b95154922e03f430b5e1a45f
tkameyamax <tkameyamax@gmail.com>
2021-02-03 22:34:38 +0900

    cors uri をenv に

--- server.go
+++ server.go
@@ -38,7 +38,7 @@ func main() {
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080", "http://localhost:8000"},
+		AllowedOrigins:   []string{os.Getenv("CORS_GRAPHQL_PLAYGROUND_URI"), os.Getenv("CORS_BLOG_URI")},
 		AllowCredentials: true,
 		Debug:            true,
 	}).Handler)
